3aeb8232bcb8a0e1e58daa266fb9d19e65a7d1e3
root <root@c027530cc1af>
2025-07-23 04:47:26 +0000

    🎨 Add frontend code and user interface

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">Legal Case Management System</h1>
+      {/* Add components for client intake, document management, etc. here */}
+    </div>
+  );
+};
+
+export default HomePage;
